b47b0a58e11f51eb32fed9c0befe2ca6f35730e4
kshitij jha <84563720+trivialguy@users.noreply.github.com>
2023-10-17 17:34:50 +0530

    Update views.py

--- covenant/transactions/views.py
+++ covenant/transactions/views.py
@@ -44,14 +44,14 @@ abi = json.loads(compiled_sol["contracts"]["Main.sol"]["Main"]["metadata"])["out
 
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
-p_address = "0x8906573010E6eD14faac8aBd6ce7B1A96aA54529"
-private_key = "0xafa25fba4612488ec2bfc7ed00da3ccd1dd8f511d6a01203588307e9eccbd9a7" # leaving the private key like this is very insecure if you are working on real world project
+p_address = "0xE4A7c8A616f125D5Cc2f6fDB334D5B89A07DAd57"
+private_key = "0x19966923ac2e4649a35a6af2827a58aae93a5ca79f32fcb8cb7b13782e4baf83" # leaving the private key like this is very insecure if you are working on real world project
 # Create the contract in Python
 Main = w3.eth.contract(abi=abi, bytecode=bytecode)
 # Get the number of latest transaction
 nonce = w3.eth.getTransactionCount(p_address)
 
-transac = Main.constructor(2,p_address).buildTransaction(
+transac = Main.constructor(2000000000000000000,p_address).buildTransaction(
     {
         "chainId": chain_id,
         "gasPrice": w3.eth.gas_price,
@@ -68,75 +68,96 @@ transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
 print("Waiting for transaction to finish...")
 transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
 print(f"Done! Contract deployed to {transaction_receipt.contractAddress}")
-
+print(f"Transaction mined in block {transaction_receipt['blockNumber']}")
 Main_list = w3.eth.contract(address=transaction_receipt.contractAddress, abi=abi)
 
 
 #define pk as map of address to private key for testing
-pk=[]
+pk={"0xE4A7c8A616f125D5Cc2f6fDB334D5B89A07DAd57":"0x19966923ac2e4649a35a6af2827a58aae93a5ca79f32fcb8cb7b13782e4baf83"}
 def transaction(request):
     if request.method=='POST':
         vote=request.POST['vote']
         current_user_address=request.user.first_name
         secret_key=get_random_secret_key()
+        print(vote)
         temp=str(hash(vote+"#"+current_user_address+"#"+secret_key))
+        print(temp)
         new_transaction=transactions.objects.create(Vote=temp,secret_key=secret_key,address=current_user_address)
-        
-        option=Main_list.functions.vote(temp).buildTransaction({"chainId": chain_id, "from": current_user_address, "gasPrice": w3.eth.gas_price, "nonce": nonce + 1})
-        
-        # Sign the transaction
-        sign_option = w3.eth.account.sign_transaction(
-            option, private_key=pk[current_user_address]
-        )
-        # Send the transaction
-        send_option = w3.eth.send_raw_transaction(sign_option.rawTransaction)
+
+        txn_hash = Main_list.functions.vote(temp).transact({'from': current_user_address, 'value': w3.toWei(2, 'ether'), 'gas': 200000, 'gasPrice': w3.toWei('50', 'gwei')})
         try:
-            transaction_receipt = w3.eth.wait_for_transaction_receipt(send_option)
-        except:
+            receipt = w3.eth.waitForTransactionReceipt(txn_hash, timeout=300)
+            print(receipt.status)
+        except ValueError as e:
+            # Handle the exception here
+            print(f"An error occurred: {e}")
             print("transaction failed")
             return redirect('transaction')
-        
-        new_transaction.save()
-        return redirect('/')
+        print(f"Transaction mined in block {receipt['blockNumber']}")
+        if receipt.status == 1:
+            new_transaction.save()
+            print("transaction successful")
+            length_arr=Main_list.functions.arr_length().call()
+            print(length_arr,"lol")
+            return redirect('/')
+        else:
+            print("transaction failed")
+            return redirect('transaction')
     else:
         return render(request,'transactions.html')
     
 def count(request):
-    current_user_address=request.user.first_name
-    length_arr=Main_list.functions.arr_length().call()
-    ind=Main_list.functions.curr_length().call()
-    mydata=transactions.objects.filter(id>=ind,id<=length_arr).values()
-    for i in range (ind,length_arr+1):
-        vote_a=hash("A"+"#"+mydata[i]['address']+"#"+mydata[i]['secret_key'])
-        vote_b=hash("B"+"#"+mydata[i]['address']+"#"+mydata[i]['secret_key'])
-        option=Main_list.functions.countvote(vote_a,vote_b,i).buildTransaction({"chainId": chain_id, "from": current_user_address, "gasPrice": w3.eth.gas_price, "nonce": nonce + 1})
+    if request.method=='POST':
+        current_user_address=request.user.first_name
+        length_arr=Main_list.functions.arr_length().call()
+        ind=Main_list.functions.curr_length().call()
+        print(ind,length_arr)
+        mydata=transactions.objects.all()[ind:length_arr]
+        
+        for i in range (ind+1,length_arr+1):
+            vote_a=hash("A"+"#"+mydata[i-1].address+"#"+mydata[i-1].secret_key)
+            vote_b=hash("B"+"#"+mydata[i-1].address+"#"+mydata[i-1].secret_key)
+            print(vote_a,vote_b)
             
-        # Sign the transaction
-        sign_option = w3.eth.account.sign_transaction(
-            option, private_key=pk[current_user_address]
-        )
-        # Send the transaction
-        send_option = w3.eth.send_raw_transaction(sign_option.rawTransaction)
-        try:
-            transaction_receipt = w3.eth.wait_for_transaction_receipt(send_option)
-        except:
-            print("transaction failed")
-            return redirect('transaction')
-    for i in range (ind,length_arr+1):
-        vote_a=hash("A"+"#"+mydata[i]['address']+"#"+mydata[i]['secret_key'])
-        vote_b=hash("B"+"#"+mydata[i]['address']+"#"+mydata[i]['secret_key'])
-        option=Main_list.functions.vote_compare(vote_a,vote_b,i,mydata[i]['address']).buildTransaction({"chainId": chain_id, "from": current_user_address, "gasPrice": w3.eth.gas_price, "nonce": nonce + 1})
+            function_selector = Main_list.encodeABI(fn_name='countvote', args=[str(vote_a),str(vote_b),i])
+            gas_estimate = w3.eth.estimate_gas({'from': current_user_address, 'data': function_selector})
+            gas_limit = gas_estimate + 50000
+            txn_hash=Main_list.functions.countvote(str(vote_a),str(vote_b),i).transact({'from': current_user_address, 'gas': gas_limit, 'gasPrice': w3.toWei('50', 'gwei')})
+            try:
+                receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+                if receipt.status==1:
+                    print("transaction success")
+                else:
+                    print("transaction terminated")
+            except ValueError as e:
+                # Handle the exception here
+                print(f"An error occurred: {e}")
+                print("transaction failed")
+                return redirect('transaction')
+        print(ind,length_arr)
+        for i in range (ind+1,length_arr+1):
+            vote_a=hash("A"+"#"+mydata[i-1].address+"#"+mydata[i-1].secret_key)
+            vote_b=hash("B"+"#"+mydata[i-1].address+"#"+mydata[i-1].secret_key)
+            print(vote_a,vote_b)
+            function_selector = Main_list.encodeABI(fn_name='vote_compare', args=[str(vote_a),str(vote_b),i,mydata[i-1].address])
+            gas_estimate = w3.eth.estimate_gas({'from': current_user_address, 'data': function_selector})
+            gas_limit = gas_estimate + 50000
+            txn_hash=Main_list.functions.vote_compare(str(vote_a),str(vote_b),i,mydata[i-1].address).transact({'from': current_user_address, 'gas': gas_limit, 'gasPrice': w3.toWei('50', 'gwei')})
             
-        # Sign the transaction
-        sign_option = w3.eth.account.sign_transaction(
-            option, private_key=pk[current_user_address]
-        )
-        # Send the transaction
-        send_option = w3.eth.send_raw_transaction(sign_option.rawTransaction)
-        try:
-            transaction_receipt = w3.eth.wait_for_transaction_receipt(send_option)
-        except:
-            print("transaction failed")
-            return redirect('transaction')
+                
+            try:
+                receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+                # print(receipt)
+                if receipt.status==1:
+                    print("transaction success")
+                else:
+                    print("transaction terminated")
+            except ValueError as e:
+                print(f"An error occurred: {e}")
+                print("transaction failed")
+                return redirect('transaction')
+        return redirect('/')
+    else:
+        return render(request,'transactions.html')
     
 
