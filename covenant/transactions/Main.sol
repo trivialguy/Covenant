1819696a59d6a36e7c7e50b9941e66cc5a422c90
kshitij jha <84563720+trivialguy@users.noreply.github.com>
2023-10-17 17:35:31 +0530

    Update Main.sol

--- covenant/transactions/Main.sol
+++ covenant/transactions/Main.sol
@@ -13,36 +13,41 @@ contract Main{
     uint256 ind=0;
     address payable admin;
     constructor(uint256 _deno, address payable _admin){
-        deno=_deno;
-        admin=_admin;
+        deno = _deno;
+        admin = _admin;
     }
-    string [] public arr;
+    string[] public arr;
     function deposit() public payable returns (uint256){
         return address(this).balance;
     }
     function arr_length() public view returns (uint256){
         return arr.length;
     }
+    function deno_return() public view returns (uint256){
+        return deno;
+    }
     function curr_length() public view returns (uint256){
         return ind;
     }
-    function vote(string calldata hash) public payable{
+    function vote(string calldata has) public payable returns (uint256){
         require(msg.value==deno,"");
-        arr.push(hash);
+        arr.push(has);
+        return arr.length;
     }
     function countvote(string calldata vote_A, string calldata vote_B, uint256 i) public{
-        require(msg.sender==admin,"");
+        // require(msg.sender==admin,"");
         if(compare(arr[i-1],vote_A)) red++;
         else if(compare(arr[i-1],vote_B)) blue++;
     }
     function vote_compare(string calldata vote_A, string calldata vote_B, uint256 i, address payable voter) public{
-        require(msg.sender==admin,"");
-        uint256 n=arr.length;
+        // require(msg.sender==admin,"");
         if(red>blue)
-        if(compare(arr[i-1],vote_A)) voter.transfer(deno*(red+blue)/red);
-        else if(blue>red)
         if(compare(arr[i-1],vote_B)) voter.transfer(deno*(red+blue)/blue);
-        ind=n;
+        else if(blue>red)
+        if(compare(arr[i-1],vote_A)) voter.transfer(deno*(red+blue)/red);
+        ind = i;
+
+
     }
     function empty_balance() public{
         require(msg.sender==admin,"");
